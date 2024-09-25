ed83582a95ee6720b147ee2b761e082290ce5dde
Tornike <tmaisuradze156@gmail.com>
2024-09-25 21:03:24 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+     <div className='aboutme'>
+      <h1>Firstname:Tornike</h1>
+      <h1>Lastname:Maisuradze</h1>
+      <h1>Age:16</h1>
+      <h1>Hobby:playing videogames</h1>
+     </div>
+  )
+}
+
+export default App
